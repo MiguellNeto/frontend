1787f35a3f79d03367349b2edea6d269f135e7ca
MigueNeto <56120625+MiguellNeto@users.noreply.github.com>
2021-03-27 17:28:04 -0300

    subindo frent

---
+++ src/app/contact-list/contact-list.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+
+
+@Component({
+  selector: 'app-contact-list',
+  templateUrl: './contact-list.component.html',
+  styleUrls: ['./contact-list.component.css']
+})
+export class ContactListComponent implements OnInit {
+
+
+  contacts: { id: any; name: any; description: any; email: any; } []| undefined;
+  selectedContact: any
+  edicao = false;
+
+  constructor(public dataService: DataService) { }
+
+  ngOnInit() {
+    this.listar();
+  }
+
+  public listar(){
+    this.dataService.getContacts().subscribe(resposta => {
+      this.contacts = resposta;
+    });    
+  }
+
+  public selectContact(contact: any) {
+    this.selectedContact = contact;
+  }
+
+    public deleteContact(contact: { id: any; }) {
+      
+      var excluir = confirm("Deseja realmente excluir este registro ?")
+      if (excluir == true){
+        this.dataService.deleteContacts(contact.id).subscribe(r => {
+          alert("Deletado com sucesso!");
+          this.listar();
+        });
+      }else{
+       alert("Operação Cancelada")
+      }
+      return this.listar();
+    }
+  
+
+  public editar(){
+    this.edicao = true;
+  }
+
+  public cancelar(){
+    this.edicao = false;
+  }
+
+  public salvar(){
+    this.dataService.saveContact(this.selectedContact).subscribe(r => {
+      this.edicao = false;
+      this.listar();
+    });
+  }
+
+
+}
---
+++ src/app/data.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+
+  constructor(private http: HttpClient) { }
+
+
+  public getContacts(): Observable<any> {
+    return this.http.get(`${environment.url}/contact`);
+  }
+
+  public deleteContacts(id: any): Observable<any> {
+    return this.http.delete(`${environment.url}/contact/${id}`);
+  }
+
+  public saveContact(contact: { id: any; name: any; description: any; email: any; }): Observable<any> {
+    return this.http.post(`${environment.url}/contact`, contact);
+  }
+
+}
